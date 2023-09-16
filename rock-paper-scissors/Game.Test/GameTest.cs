6c1a153c68518f3f8fe636885726c5d584e5123a
Félix Díez Arias <fdiez@tunstall.es>
2023-09-16 09:49:55 +0200

    RED: scissors_should_win_against_paper

--- rock-paper-scissors/Game.Test/GameTest.cs
+++ rock-paper-scissors/Game.Test/GameTest.cs
@@ -25,5 +25,38 @@ namespace Game.Test
             // Assert
             gameStatus.Should().Be(GameStatus.Player2Won);
         }
+
+        [Fact]
+        public void scissors_should_win_against_paper()
+        {
+            // Arrange
+            // Act
+            var gameStatus = RockPaperScissors.Play(Moves.Scissors, Moves.Paper);
+
+            // Assert
+            gameStatus.Should().Be(GameStatus.Player1Won);
+        }
+
+        //[Fact]
+        //public void paper_should_win_against_rock()
+        //{
+        //    // Arrange
+        //    // Act
+        //    var gameStatus = RockPaperScissors.Play(Moves.Paper, Moves.Rock);
+
+        //    // Assert
+        //    gameStatus.Should().Be(GameStatus.Player1Won);
+        //}
+
+        //[Fact]
+        //public void rock_should_loose_against_paper()
+        //{
+        //    // Arrange
+        //    // Act
+        //    var gameStatus = RockPaperScissors.Play(Moves.Rock, Moves.Paper);
+
+        //    // Assert
+        //    gameStatus.Should().Be(GameStatus.Player1Won);
+        //}
     }
 }
