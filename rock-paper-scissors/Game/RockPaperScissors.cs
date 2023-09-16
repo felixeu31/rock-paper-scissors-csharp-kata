6a63574df2053aebbdcfcdd3b10e2a81b098527f
Félix Díez Arias <fdiez@tunstall.es>
2023-09-16 09:50:54 +0200

    GREEN: scissors_should_win_against_paper

--- rock-paper-scissors/Game/RockPaperScissors.cs
+++ rock-paper-scissors/Game/RockPaperScissors.cs
@@ -5,7 +5,13 @@ public class RockPaperScissors
     public static GameStatus Play(Moves movePlayer1, Moves movePlayer2)
     {
         if (movePlayer1 == Moves.Scissors)
+        {
+            if (movePlayer2 == Moves.Paper)
+            {
+                return GameStatus.Player1Won;
+            }
             return GameStatus.Player2Won;
+        }
 
         return GameStatus.Player1Won;
     }
