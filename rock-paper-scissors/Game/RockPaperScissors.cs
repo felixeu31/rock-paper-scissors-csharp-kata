02c308540599eb51ede4d46806188813f74ef5b9
Félix Díez Arias <fdiez@tunstall.es>
2023-09-16 09:31:03 +0200

    GREEN: rock_should_win_scissors

--- rock-paper-scissors/Game/RockPaperScissors.cs
+++ rock-paper-scissors/Game/RockPaperScissors.cs
@@ -4,6 +4,6 @@ public class RockPaperScissors
 {
     public static GameStatus Play(Moves movePlayer1, Moves movePlayer2)
     {
-        throw new NotImplementedException();
+        return GameStatus.Player1Won;
     }
 }
