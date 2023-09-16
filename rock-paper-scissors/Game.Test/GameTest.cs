a3cb5e97df4acbe3761b2f9c1ba9d777f09372ca
Félix Díez Arias <fdiez@tunstall.es>
2023-09-16 10:01:23 +0200

    REFACTOR: simplify tests with parametrized tests

--- rock-paper-scissors/Game.Test/GameTest.cs
+++ rock-paper-scissors/Game.Test/GameTest.cs
@@ -4,59 +4,20 @@ namespace Game.Test
 {
     public class GameTest
     {
-        [Fact]
-        public void rock_should_win_against_scissors()
+        [Theory]
+        [InlineData(Moves.Rock, Moves.Scissors, GameStatus.Player1Won)]
+        [InlineData(Moves.Scissors, Moves.Rock, GameStatus.Player2Won)]
+        [InlineData(Moves.Scissors, Moves.Paper, GameStatus.Player1Won)]
+        [InlineData(Moves.Paper, Moves.Scissors, GameStatus.Player2Won)]
+        [InlineData(Moves.Paper, Moves.Rock, GameStatus.Player1Won)]
+        public void run_game_should_return_expected_game_status(Moves movePlayer1, Moves movePlayer2, GameStatus expectedGameStatus)
         {
             // Arrange
             // Act
-            var gameStatus = RockPaperScissors.Play(Moves.Rock, Moves.Scissors);
+            var gameStatus = RockPaperScissors.Play(movePlayer1, movePlayer2);
 
             // Assert
-            gameStatus.Should().Be(GameStatus.Player1Won);
-        }
-
-        [Fact]
-        public void scissors_should_loose_against_rock()
-        {
-            // Arrange
-            // Act
-            var gameStatus = RockPaperScissors.Play(Moves.Scissors, Moves.Rock);
-
-            // Assert
-            gameStatus.Should().Be(GameStatus.Player2Won);
-        }
-
-        [Fact]
-        public void scissors_should_win_against_paper()
-        {
-            // Arrange
-            // Act
-            var gameStatus = RockPaperScissors.Play(Moves.Scissors, Moves.Paper);
-
-            // Assert
-            gameStatus.Should().Be(GameStatus.Player1Won);
-        }
-
-        [Fact]
-        public void paper_should_loose_against_scissors()
-        {
-            // Arrange
-            // Act
-            var gameStatus = RockPaperScissors.Play(Moves.Paper, Moves.Scissors);
-
-            // Assert
-            gameStatus.Should().Be(GameStatus.Player2Won);
-        }
-
-        [Fact]
-        public void paper_should_win_against_rock()
-        {
-            // Arrange
-            // Act
-            var gameStatus = RockPaperScissors.Play(Moves.Paper, Moves.Rock);
-
-            // Assert
-            gameStatus.Should().Be(GameStatus.Player1Won);
+            gameStatus.Should().Be(expectedGameStatus);
         }
 
         //[Fact]
