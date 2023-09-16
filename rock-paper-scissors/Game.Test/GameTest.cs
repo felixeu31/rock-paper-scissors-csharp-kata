a931962df7c975ba64fe37d97aecd5ecfaf7ccb6
Félix Díez Arias <fdiez@tunstall.es>
2023-09-16 10:02:16 +0200

    RED:  [InlineData(Moves.Rock, Moves.Paper, GameStatus.Player2Won)]

--- rock-paper-scissors/Game.Test/GameTest.cs
+++ rock-paper-scissors/Game.Test/GameTest.cs
@@ -10,6 +10,7 @@ namespace Game.Test
         [InlineData(Moves.Scissors, Moves.Paper, GameStatus.Player1Won)]
         [InlineData(Moves.Paper, Moves.Scissors, GameStatus.Player2Won)]
         [InlineData(Moves.Paper, Moves.Rock, GameStatus.Player1Won)]
+        [InlineData(Moves.Rock, Moves.Paper, GameStatus.Player2Won)]
         public void run_game_should_return_expected_game_status(Moves movePlayer1, Moves movePlayer2, GameStatus expectedGameStatus)
         {
             // Arrange
@@ -19,16 +20,5 @@ namespace Game.Test
             // Assert
             gameStatus.Should().Be(expectedGameStatus);
         }
-
-        //[Fact]
-        //public void rock_should_loose_against_paper()
-        //{
-        //    // Arrange
-        //    // Act
-        //    var gameStatus = RockPaperScissors.Play(Moves.Rock, Moves.Paper);
-
-        //    // Assert
-        //    gameStatus.Should().Be(GameStatus.Player1Won);
-        //}
     }
 }
